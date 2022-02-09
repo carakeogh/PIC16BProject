4f2cd50e81346e506a81e1386e6d6eea6464205d
Patrick Wang <wangpatrick2001@gmail.com>
2022-02-09 00:12:24 -0800

    updates to webapp

--- app.py
+++ app.py
@@ -5,12 +5,23 @@ app = Flask(__name__)
 def main():
     return render_template("homepage.html")
 
-@app.route("/start/", methods = ["POST", "GET"])
+
+@app.route("/", methods = ["POST", "GET"])
 def start():
     if request.method == "GET":
         return render_template("start.html")
+
     else:
         try:
-            return render_template("start.html", guru=request.form["guru"])
+            return render_template("start.html", name = request.form['name'])
         except:
-            return render_template("start.html")
+            return render_template("recommender.html")
+
+
+@app.route("/", methods = ["POST", "GET"])
+def recommender():
+    if request.method == "GET":
+        return render_template("recommender.html")
+
+    else:
+        return render_template("recommender.html")
