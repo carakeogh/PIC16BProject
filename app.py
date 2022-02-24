69003c0a3b94e87fdd84a75d2c77a5b7e4b1a7ac
yuhsin-huang <97138478+yuhsin-huang@users.noreply.github.com>
2022-02-23 18:36:20 -0800

    Connect dataframes to webapp and allow webapp to show images

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask, render_template, request
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 app = Flask(__name__)
+
+def getData(type1):
+    df = pd.read_csv("styles.csv", nrows=10000, error_bad_lines=False)
+    image_df = pd.read_csv("images.csv")
+    df['filename'] = df.apply(lambda row: str(row['id']) + ".jpg", axis=1)
+
+    # merge images.csv and styles.csv together
+    df = pd.merge(df, image_df, on = ["filename"])
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    return df[df['subCategory'] == type1]
+
+def randomSelect(df, k):
+    return random.sample(set(df["link"]), k)
 
 @app.route("/")
 def main():
@@ -24,16 +43,19 @@ def chooseType(name):
 
     else:
         try:
-            return render_template("recommender.html", type=request.form['type'])
+            return render_template("chooseType.html", type1=request.form['type1'])
         except:
-            return render_template("recommender.html")
+            return render_template("chooseType.html")
 
 
 
-@app.route("/recommender/<name>/", methods = ["POST", "GET"])
-def recommender(name):
+@app.route("/<type1>/", methods = ["POST", "GET"])
+def recommender(type1):
+    df = getData(type1)
+    images = randomSelect(df, 30)
+
     if request.method == "GET":
-        return render_template("recommender.html")
+        return render_template("recommender.html", images = images)
 
     else:
-        return render_template("recommender.html")
+        return render_template("recommender.html", images = images)
