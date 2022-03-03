a60a8db8a5a568d81ce44f609b49943ad0bde908
Patrick Wang <wangpatrick2001@gmail.com>
2022-03-03 14:47:01 -0800

    updates to css

--- app.py
+++ app.py
@@ -1,29 +1,15 @@
 from flask import Flask, render_template, request
-#import random
+
 import matplotlib.pyplot as plt
 import numpy as np
-#import pandas as pd
 import functions
 
 app = Flask(__name__)
 
-#def getData(type1):
-   # df = pd.read_csv("styles.csv", nrows=10000, error_bad_lines=False)
-   # image_df = pd.read_csv("images.csv")
-   # df['filename'] = df.apply(lambda row: str(row['id']) + ".jpg", axis=1)
-
-    # merge images.csv and styles.csv together
-    #df = pd.merge(df, image_df, on = ["filename"])
-   # df = df.sample(frac=1).reset_index(drop=True)
-
-   # return df[df['subCategory'] == type1]
-
-#def randomSelect(df, k):
-  #  return random.sample(set(df["link"]), k)
-
 @app.route("/")
 def main():
     return render_template('base.html')
+
 
 @app.route("/homepage/", methods = ["POST", "GET"])
 def homepage():
